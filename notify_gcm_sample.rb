d5c000d3d32cdaa4d91f3073e7f2b651d301949b
Kaushik Gopal <code@kaush.co>
2013-10-30 14:47:07 -0700

    Added some helpful hints for the push to gcm server script

--- notify_gcm_sample.rb
+++ notify_gcm_sample.rb
@@ -1,8 +1,21 @@
+###################################################
+# A simple ruby script to push notifications to GCM
+###################################################
+
+# uses the gem from https://github.com/spacialdb/gcm
+# gem install gcm
 require 'gcm'
+
+# an array of one or more client registration IDs
+registration_ids= ["asdfasdfasdfa"]
+
 API_SERVER_KEY = "ENTER_API_KEY_HERE"
 
 gcm = GCM.new(API_SERVER_KEY)
 
-registration_ids= ["asdfasdfasdfa"] # an array of one or more client registration IDs
-options = {data: {score: Time.new.to_s}, collapse_key: "updated_score"}
+options = { data: {score: Time.new.to_s}, collapse_key: "updated_score"}
+
 response = gcm.send_notification(registration_ids, options)
+
+# Run above script as follows
+# ruby -rubygems notify_gcm.rb
